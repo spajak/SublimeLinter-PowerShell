0d240c5fabcb0ecb3bdabf0822d72272a3ba94e0
Sebastian Pająk <spconv@gmail.com>
2021-03-14 13:27:33 +0100

    First working version

--- linter.py
+++ linter.py
@@ -1,10 +1,29 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter
+from base64 import b64encode
 
-
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
+class pwsh(Linter):
+    analyzer_cmd = (
+        'Invoke-ScriptAnalyzer -ScriptDefinition ($input | Out-String) | '
+        'Select-Object -Property Line,Message,Severity,Column,RuleName | '
+        'ConvertTo-Csv -Delimiter "\t" -QuoteFields False | '
+        'Select-Object -Skip 1'
+    )
+    regex = (
+        r'(?P<line>\d+)\t(?P<message>[^\t]+)\t'
+        r'((?P<error>ParseError|Error)|(?P<warning>Warning|Information))\t'
+        r'(?P<col>\d*)\t(?P<rule>[^\t]*)'
+    )
     multiline = False
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.powershell'
     }
+    cmd = [
+        'pwsh',
+        '-EncodedCommand',
+        b64encode(analyzer_cmd.encode('utf_16_le')).decode('ascii')
+    ]
+    def split_match(self, match):
+        result = super().split_match(match)
+        if result.rule:
+            result.message += ' (Rule: {0})'.format(result.rule)
+        return result
